d76236552bcab9660e0f636f0b4151ed5dd6e4f7
Milan <milandhungana131@gmail.com>
2024-04-04 23:03:36 +0545

    icons

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ const date = currentDate.toLocaleDateString('en-US', { month: '2-digit', day: '2
 const currentTime = currentDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
 const amOrPm = currentTime.slice(-2);
 
-const image = 'day.jpg';
-
 
 document.getElementById('today').innerText = `${day}, ${date} ${currentTime}`;
 
-const cloud_image = 'cloudy.png';
-const sun_image = 'sun.png';
-const rain_image = 'rain.png';
 const API_KEY = 'a9c001c6a7b8dc2d8044a547c5fac590';
 const search = document.getElementById('search');
 const cityName = document.getElementById('city_name');
 const temp = document.getElementById('temp');
 const humidity = document.getElementById('humidity');
 const wind = document.getElementById('wind');
+const pressure = document.getElementById('pressure');
+
+const weatherIcon = document.getElementById('weatherIcon');
+
+
 
 search.addEventListener('click', () => {
   const city = document.querySelector('input').value;
@@ -29,21 +29,15 @@ async function makeRequest(city){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
     const data = await response.json();
     console.log(data);
-    cityName.innerText = data.name;
-    temp.innerText = `${Math.round(data.main.temp - 273.15)}°C / ${Math.round((data.main.temp - 273.15) * 9/5 + 32)}°F`;
+    city_name.innerText = city
+    temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°C / ${Math.round((data.main.temp - 273.15) * 9/5 + 32)}°F`
+    var icon_code = data.weather[0].icon
+    var icon_url = ` https://openweathermap.org/img/wn/${icon_code}@2x.png`
+    weatherIcon.src = icon_url;
+
     humidity.innerText = `${data.main.humidity}%`;
-    wind.innerText = `${data.wind.speed} km/h`;
-    console.log(data.weather[0].main);
-    if (data.weather[0].main === 'Clouds') {
-      document.getElementById('weather_image').src = cloud_image;
-    } else if (data.weather[0].main === 'Clear') {
-      document.getElementById('weather_image').src = sun_image;
-    } else if (data.weather[0].main === 'Rain') {
-      document.getElementById('weather_image').src = rain_image;
-    }
-    else if ( data.weather[0].main === 'Thunderstorm') {
-      document.getElementById('weather_image').src = 'thunderstorm.png';
-    }
+    wind.innerText = `${data.wind.speed} km/h`; 
+    pressure.innerText = `${data.main.pressure}`; 
   }
   catch (error){
     console.log(error)
