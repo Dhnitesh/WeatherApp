79672743413deb107d6c4054a58a08422924ad90
Milan <milandhungana131@gmail.com>
2024-04-07 20:54:43 +0545

    fix

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const currentDate = new Date();
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const day = days[currentDate.getDay()];
 const date = currentDate.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: '2-digit' });
-const currentTime = currentDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-const amOrPm = currentTime.slice(-2);
 
-
-document.getElementById('today').innerText = `${day}, ${date} ${currentTime}`;
+document.getElementById('today').innerText = date;
 
 const API_KEY = 'a9c001c6a7b8dc2d8044a547c5fac590';
 const search = document.getElementById('search');
@@ -18,8 +13,6 @@ const pressure = document.getElementById('pressure');
 
 const weatherIcon = document.getElementById('weatherIcon');
 
-
-
 search.addEventListener('click', () => {
   const city = document.querySelector('input').value;
   makeRequest(city)
@@ -28,7 +21,7 @@ async function makeRequest(city){
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
     const data = await response.json();
-    console.log(data);
+    console.log("pppppppppp",data);
     city_name.innerText = city
     temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°C / ${Math.round((data.main.temp - 273.15) * 9/5 + 32)}°F`
     var icon_code = data.weather[0].icon
